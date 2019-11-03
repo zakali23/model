511cd7e1653733c1f736d8a9ffec73b591ae7e1a
Zakali23 <zakaria.zekraoui@gmail.com>
2019-11-03 21:57:57 +0100

    create app in vue js

---
+++ assets/js/App.vue
@@ -0,0 +1,31 @@
+<template>
+<v-app>
+    <v-alert
+      v-model="alert"
+      dismissible
+      color="success"
+      border="left"
+      elevation="2"
+      colored-border
+      icon="mdi-check-all"
+    >
+     Votre Application à été bien <strong> installé</strong>.
+    </v-alert>
+</v-app>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data () {
+      return {
+        alert: true,
+      }
+    }
+ 
+};
+</script>
+
+<style>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+ 
+    .addEntry('app', './assets/js/main.js')
+
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // enables @babel/preset-env polyfills
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    })
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+    .addEntry('style', './assets/css/app.css')
+    // enable vue js
+    .enableVueLoader(() => {}, {
+                 useJsx: true
+                          })
+;
+
+module.exports = Encore.getWebpackConfig();
